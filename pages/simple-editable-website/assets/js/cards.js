6c5fa56de1593db2bca066cab22f60f01caa3d30
Israel Senpai <90741571+IsraelSenpai@users.noreply.github.com>
2022-10-10 20:13:04 -0300

    Delete pages/simple-editable-website directory

--- pages/simple-editable-website/assets/js/cards.js
+++
@@ -1,9 +0,0 @@
-const elements = document.querySelectorAll('.card-grid .card a .card-content .content p')
-const limit = 50
-
-for (let p of elements)
-{
-    const aboveLimit = p.innerText.length > limit
-    const dots0rEmpty = aboveLimit ? '...' : ''
-    p.innerText = p.innerText.substring(0, limit) + dots0rEmpty
-}
